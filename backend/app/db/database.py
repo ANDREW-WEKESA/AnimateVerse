d0d172145d6e3731fc31c6a03b479da6a136548b
ANDREW-WEKESA <andrewwekesa675@gmail.com>
2026-07-15 15:39:54 +0300

    feat(db): configure PostgreSQL engine

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,2 +1,12 @@
+from os import getenv
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
-Base=declarative_base()
+
+load_dotenv()
+
+DATABASE_URL = getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL, echo=True)
+
+Base = declarative_base()
